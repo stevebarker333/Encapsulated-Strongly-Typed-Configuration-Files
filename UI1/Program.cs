11f33a4ee26c15745c3e5b1f503c93997cc476f2
Steve Barker <steve.barker2@pearson.com>
2013-03-27 22:05:16 +0000

    Code created and checked-in.

---
+++ UI1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UI1.Configuration;
+using API;
+
+namespace UI1
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var configurationFile = new ConfigurationFile();
+
+            Console.WriteLine(configurationFile.UI1String);
+            Console.WriteLine(configurationFile.UI1Int);
+            Console.WriteLine(configurationFile.UI1Uri);
+
+            var widget = new Widget(configurationFile);
+            widget.DoSomething();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UI2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UI2.Configuration;
+using API;
+
+namespace UI2
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var configurationFile = new ConfigurationFile();
+
+            Console.WriteLine(configurationFile.UI2String);
+            Console.WriteLine(configurationFile.UI2Int);
+            Console.WriteLine(configurationFile.UI2Uri);
+
+            var widget = new Widget(configurationFile);
+            widget.DoSomething();
+
+            Console.ReadLine();
+        }
+    }
+}
